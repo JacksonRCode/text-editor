3f6d469c3480ead957e7945c4bcf5ac636cbf500
JacksonRCode <jackdareid@gmail.com>
2024-05-20 18:51:25 -0400

    Error handling
    Implement die() function that prints error message
    and exits the program.
    - perror() looks at global errno var and prints
      descriptive error message for it. Also prints
      str given to it
    
    tcsetattr(),tcgetattr(), and read() all return -1
    on failure.

--- kilo.c
+++ kilo.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
@@ -7,17 +8,26 @@
 // This will store the original state of the terminal
 struct termios orig_termios;
 
+void die(const char *s) {
+    /*
+    Print error message and exit
+    */
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode(void) {
     /*
     orig_termios is the original terminal settings that are
     reapplied when the program exits
     */
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) 
+        die("tcsetattr");
 }
 
 void enableRawMode(void) {
     // Read current terminal attributes into struct orig_termios
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     
     // atexit() is called automatically when the program exits
     atexit(disableRawMode);
@@ -31,16 +41,15 @@ void enableRawMode(void) {
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
         
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
 int main(void) {
     enableRawMode();
     
-    char c;
     while (1) {
         char c = '\0';
-        read(STDIN_FILENO, &c, 1);
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if (iscntrl(c)) {
             // Prints ascii codes of control characters
             printf("%d\n", c);
