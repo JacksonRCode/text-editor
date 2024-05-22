9b551a3d01726832b0c84507f9c75f9cd6971220
JacksonRCode <jackdareid@gmail.com>
2024-05-22 12:08:10 -0400

    Complete getWindowSize
    We get the cursor position by writing 6n to
    STDOUT_FILENO and then we read this using
    STDIN_FILENO and look for 'R'. Before this R
    is the row and col #'s

--- kilo.c
+++ kilo.c
@@ -99,27 +99,32 @@ void editorProcessKeypress(void) {
 
 int getCursorPosition(int *rows, int *cols) {
     // escape sequence 6n retrieves & sends cursor pos. to stdout
+
+    char buf[32];
+    unsigned int i = 0;
+
     if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
 
-    printf("\r\n");
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1) {
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
     }
 
-    editorReadKey();
+    buf[i] = '\0';
 
-    return -1;
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    // parses two integers separated by a ; and put the values
+    // into rows and cols variables
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
 }
 
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
 
-    if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
         // This line moves cursor right 999 (B) 
         // Also moves cursor down 999 (C)
         if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
