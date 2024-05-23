ec9b75fa7a1cb2c9c30e634a7c5a6c9324491a31
JacksonRCode <jackdareid@gmail.com>
2024-05-23 07:28:30 -0400

    add special keys (del, home, end, pageup, pagedwn)

--- kilo.c
+++ kilo.c
@@ -15,10 +15,16 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
+    // set to high value so they don't interfere with any char vals
     ARROW_LEFT = 1000,
     ARROW_RIGHT, // 1001
     ARROW_UP, // 1002
-    ARROW_DOWN // 1003
+    ARROW_DOWN, // 1003
+    DEL_KEY,
+    HOME_KEY,
+    END_KEY,
+    PAGE_UP,
+    PAGE_DOWN
 };
 
 /*** Data ***/
@@ -94,17 +100,40 @@ int editorReadKey(void) {
 
         
         if (seq[0] == '[') {
+            if (seq[1] >= '0' && seq[1] <= '9') {
+                // page up and down are esc[~5 and esc[~6 which is what we are checking for
+                if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if (seq[2] == '~') {
+                    switch (seq[1]) {
+                        case '1': return HOME_KEY;
+                        case '3': return DEL_KEY;
+                        case '4': return END_KEY;
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
+                    }
+                }
+            } else {
+                switch (seq[1]) {
+                    case 'A': return ARROW_UP;
+                    case 'B': return ARROW_DOWN;
+                    case 'C': return ARROW_RIGHT;
+                    case 'D': return ARROW_LEFT;
+                    case 'H': return HOME_KEY;
+                    case 'F': return END_KEY;
+                }
+            }
+        } else if (seq[0] == 'O') {
             switch (seq[1]) {
-                case 'A': return ARROW_UP;
-                case 'B': return ARROW_DOWN;
-                case 'C': return ARROW_RIGHT;
-                case 'D': return ARROW_LEFT;
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
             }
         }
 
         return '\x1b';
-    }   else {
-            return c;
+    } else {
+        return c;
     }
 }
 
@@ -259,12 +288,30 @@ void editorProcessKeypress(void) {
             exit(0);
             break;
 
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+
+        case END_KEY:
+            E.cx = E.screenCols - 1;
+            break;
+
+        case PAGE_UP:
+        case PAGE_DOWN:
+            {
+                int times = E.screenRows;
+                while (times--)
+                    editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+            }
+            break;
+
         case ARROW_UP:
         case ARROW_DOWN:
         case ARROW_LEFT:
         case ARROW_RIGHT:
             editorMoveCursor(c);
             break;
+
     }
 }
 
