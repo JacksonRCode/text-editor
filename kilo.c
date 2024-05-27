914d6bf15c2f03e4ab3d59fd637fc0e76d02a9fa
JacksonRCode <jackdareid@gmail.com>
2024-05-27 10:24:15 -0400

    Created saving functionality.

--- kilo.c
+++ kilo.c
@@ -70,6 +70,8 @@ struct editorConfig E;
 /*** Prototypes ***/
 
 void editorSetStatusMessage(const char *fmt, ...);
+void editorRefreshScreen(void);
+char *editorPrompt(char *prompt);
 
 /*** Terminal ***/
 
@@ -409,7 +411,13 @@ void editorOpen(char *filename) {
 }
 
 void editorSave(void) {
-    if (E.fileName == NULL) return;
+    if (E.fileName == NULL) {
+        E.fileName = editorPrompt("Save as: %s");
+        if (E.fileName == NULL) {
+            editorSetStatusMessage("Save aborted");
+            return;
+        }
+    }
 
     int len;
     char *buf = editorRowsToString(&len);
@@ -603,6 +611,41 @@ void snapCursorX(void) {
 }
 
 /*** Input ***/
+
+char *editorPrompt(char *prompt) {
+    size_t bufsize = 128;
+    char *buf = malloc(bufsize);
+
+    size_t buflen = 0;
+    buf[0] = '\0';
+
+    while (1) {
+        editorSetStatusMessage(prompt, buf);
+        editorRefreshScreen();
+
+        int c = editorReadKey();
+        if (c == DEL_KEY || c == CTRL_KEY('h') || c == BACKSPACE) {
+            if (buflen != 0) buf[--buflen] = '\0';
+        } else if (c == '\x1b') {
+            editorSetStatusMessage("");
+            free(buf);
+            return NULL;
+        } else if (c == '\r') {
+            if (buflen != 0) {
+                editorSetStatusMessage("");
+                return buf;
+            }
+            // make sure c isn't a special character
+        } else if (!iscntrl(c) && c < 128) {
+            if (buflen == bufsize - 1) {
+                bufsize *= 2;
+                buf = realloc(buf, bufsize);
+            }
+            buf[buflen++] = c;
+            buf[buflen] = '\0';
+        }
+    }
+}
 
 void editorMoveCursor(int key) {
     // Get current row
