f19fb3d0f8ad91af1d906642de4fded508810f41
JacksonRCode <jackdareid@gmail.com>
2024-05-27 11:29:39 -0400

    Add arrow result toggle to search.

--- kilo.c
+++ kilo.c
@@ -457,15 +457,36 @@ void editorSave(void) {
 /*** Find ***/
 
 void editorFindCallback(char *query, int key) {
+    static int last_match = -1;
+    static int direction = -1;
+
     if (key == '\r' || key == '\x1b') {
+        last_match = -1;
+        direction = 1;
         return;
+    } else if (key == ARROW_RIGHT || key == ARROW_DOWN) {
+        direction = 1;
+    } else if (key == ARROW_LEFT || key == ARROW_UP) {
+        direction = -1;
+    } else {
+        last_match = -1;
+        direction = 1;
     }
+    if (last_match == -1) direction = 1;
+    int current = last_match;
+
     int i;
     for (i = 0; i < E.numRows; i++) {
-        erow *row = &E.row[i];
+        current += direction;
+        if (current == -1) current = E.numRows - 1;
+        else if (current == E.numRows) current = 0;
+
+
+        erow *row = &E.row[current];
         char *match = strstr(row->render, query);
         if (match) {
-            E.cy = i;
+            last_match = current;
+            E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowOff = E.numRows;
             break;
@@ -474,10 +495,21 @@ void editorFindCallback(char *query, int key) {
 }
 
 void editorFind(void) {
-  char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
-  if (query) {
-    free(query);
-  }
+    int saved_cx = E.cx;
+    int saved_cy = E.cy;
+    int saved_colOff = E.colOff;
+    int saved_rowOff = E.rowOff;
+
+    char *query = editorPrompt("Search: %s (ESC/Arrows/Enter)", editorFindCallback);
+    
+    if (query) {
+        free(query);
+    } else {
+        E.cx = saved_cx;
+        E.cy = saved_cy;
+        E.rowOff = saved_rowOff;
+        E.colOff = saved_colOff;
+    }
 }
 
 
